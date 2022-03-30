ad4288900f8cb1f05dad71be623b970559d277b9
CarolynnFleming <cfcatpink@gmail.com>
2022-03-29 22:51:44 -0700

    test to create a new user written as well as a amock user and register and login function

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,26 @@ const request = require('supertest');
 const app = require('../lib/app');
 const UserService = require('../lib/services/UserService');
 
+const mockUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@example.com',
+  password: '12345',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  const agent = request.agent(app);
+  
+  const user = UserService.create({ ...mockUser, ...userProps });
+
+  const { email } = user;
+  await (await agent.post('/api/v1/users/Totoros')).send({ email, password });
+  return [agent, user];
+};
+
+
 describe('alchemy-app routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -11,5 +31,16 @@ describe('alchemy-app routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+  it('creates a new user', async() => {
+    const res = await (await request(app).post('/api/v1/users')).send({ mockUser });
+    const { firstName, lastName, email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
 });
